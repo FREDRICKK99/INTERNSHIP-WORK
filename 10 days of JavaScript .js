e70c0c372d31b18db7af13a98f5af04df1414b1b
FREDRICKK99 <92713163+FREDRICKK99@users.noreply.github.com>
2021-11-02 09:17:57 +0300

    Update 10 days of JavaScript .js

--- 10 days of JavaScript .js
+++ 10 days of JavaScript .js
@@ -32,6 +32,9 @@ function greeting(parameterVariable) {
      *   secondDecimal - The string representation of a floating-point number.
      *   secondString - A string consisting of one or more space-separated words.
      **/
+    
+    
+    
     function performOperation(secondInteger, secondDecimal, secondString) {
         // Declare a variable named 'firstInteger' and initialize with integer value 4.
         const firstInteger = 4;
@@ -302,4 +305,552 @@ function greeting(parameterVariable) {
                 }
             }
             return tally;
+        }
+        
+        
+        
+        // Day 4: Classes
+        /*
+         * Implement a Polygon class with the following properties:
+         * 1. A constructor that takes an array of integer side lengths.
+         * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+         */
+        class Polygon {
+            constructor(sides) {
+                this.sides = sides;
+            }
+            perimeter() {
+                let sum = 0;
+                for (let ind = 0; ind < this.sides.length; ind++) {
+                    sum += this.sides[ind];
+                }
+                return sum;
+            }
+        }
+
+        // Day 5: Inheritance
+
+
+        /*
+         *  Write code that adds an 'area' method to the Rectangle class' prototype
+         */
+        Rectangle.prototype.area = function() {
+                return this.w * this.h;
+            }
+            /*
+             * Create a Square class that inherits from Rectangle and implement its class constructor
+             */
+        class Square extends Rectangle {
+            constructor(w) {
+                super(w, w);
+
+            }
+        }
+        // Day 5: Template Literals
+
+
+        /*
+         * Determine the original side lengths and return an array:
+         * - The first element is the length of the shorter side
+         * - The second element is the length of the longer side
+         * 
+         * Parameter(s):
+         * literals: The tagged template literal's array of strings.
+         * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+         */
+        function sides(literals, ...expressions) {
+            let area = expressions[0];
+            let perim = expressions[1];
+            let s1 = (perim + discriminant(perim, area)) / 4;
+            let s2 = (perim - discriminant(perim, area)) / 4;
+            return [s2, s1];
+
+        }
+
+        function discriminant(perim, area) {
+            return Math.sqrt(Math.pow(perim, 2) - 16 * area);
+        }
+
+        // Day 5: Arrow Functions
+
+
+        /*
+         * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+         * 
+         * Parameter(s):
+         * nums: An array of numbers.
+         */
+        function modifyArray(nums) {
+            return nums.map(elem => elem % 2 == 0 ? elem * 2 : elem * 3);
+
+        }
+        // Day 6: Bitwise Operators
+        //Today, we're practicing bitwise operations. Check the attached tutorial for more details.
+
+        // Task
+
+        // We define  to be a sequence of distinct sequential integers from  to ; in other words, . We want to know the maximum bitwise AND value of any two integers,  and  (where ), in sequence  that is also less than a given integer, .
+
+        // Complete the function in the editor so that given  and , it returns the maximum .
+
+        // Note: The  symbol represents the bitwise AND operator.
+
+
+        function getMaxLessThanK(n, k) {
+            let max = 0;
+            for (let a = n; a > 1; a--) {
+                for (let b = a - 1; b > 0; b--) {
+                    if (a & b < k && a & b > max) {
+                        max = a & b;
+                    }
+                }
+            }
+            return max
+        }
+
+        // Day 6: JavaScript Dates
+
+
+
+        // The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+        function getDayName(dateString) {
+            let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            // Write your code here
+            let day = new Date(dateString).getDay()
+
+            return dayName[day]
+        }
+
+        // Day 7: Regular Expressions I
+        // Objective
+
+        // In this challenge, we use a Regular Expression to evaluate a string. Check out the attached tutorial for more details.
+
+        // Task
+
+        // Complete the function in the editor below by returning a RegExp object, , that matches any string  that begins and ends with the same vowel. Recall that the English vowels are a, e, i, o, and u.
+
+        // Constraints
+
+        // The length of string  is  .
+        // String  consists of lowercase letters only (i.e., [a-z]).
+
+
+        function regexVar() {
+            /*
+             * Declare a RegExp object variable named 're'
+             * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+             */
+            const re = /^([ aeiou]).*\1$/;
+
+            /*
+             * Do not remove the return statement
+             */
+            return re;
+        }
+
+        // Day 7: Regular Expressions II
+
+        //         Task
+
+        // Complete the function in the editor below by returning a RegExp object, , that matches any string  satisfying both of the following conditions:
+
+        // String  starts with the prefix Mr., Mrs., Ms., Dr., or Er.
+        // The remainder of string  (i.e., the rest of the string after the prefix) consists of one or more upper and/or lowercase English alphabetic letters (i.e., [a-z] and [A-Z]).
+        // Constraints
+
+        // The length of string  is  .
+
+
+
+        function regexVar() {
+            /*
+             * Declare a RegExp object variable named 're'
+             * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+             * followed by one or more letters.
+             */
+            const re = /^(Mr|Mrs|Ms|Dr|Er)\.\w+$/;
+
+            /*
+             * Do not remove the return statement
+             */
+            return re;
+        }
+
+        // Day 7: Regular Expressions III
+
+
+
+        function regexVar() {
+            /*
+             * Declare a RegExp object variable named 're'
+             * It must match ALL occurrences of numbers in a string.
+             */
+
+            const re = /\d+/g;
+            /*
+             * Do not remove the return statement
+             */
+            return re;
+        }
+
+        // Day 8: Create a Button
+        // index . html
+
+        var btn = document.createElement("Button")
+        btn.innerHTML = "0";
+        btn.id = "btn";
+
+        document.body.appendChild(btn);
+
+        btn.onclick = function() {
+            btn.innerHTML = Number(btn.innerHTML) + 1;
+        }
+
+        // button .js
+
+        var btn = document.createElement("Button")
+        btn.innerHTML = "0";
+        btn.id = "btn";
+
+        document.body.appendChild(btn);
+
+        btn.onclick = function() {
+            btn.innerHTML = Number(btn.innerHTML) + 1;
+        }
+
+        // button  . css
+
+        #
+        btn {
+            width: 96 px;
+            height: 48 px;
+            font - size: 24 px;
+
+        }
+
+        // Day 8: Buttons Container
+
+        // index .html
+
+        <
+        !DOCTYPE html >
+            <
+            html >
+            <
+            head >
+            <
+            meta charset = "utf-8" >
+            <
+            title > Buttons Grid < /title> <
+        link rel = "stylesheet"
+        href = "css/buttonsGrid.css"
+        type = "text/css" >
+            <
+            /head>
+
+        <
+        body >
+            <
+            div id = "btns"
+        class = "btnContainer" >
+            <
+            button id = "btn1"
+        class = "btn" > 1 < /button> <
+        button id = "btn2"
+        class = "btn" > 2 < /button> <
+        button id = "btn3"
+        class = "btn" > 3 < /button> <
+        button id = "btn4"
+        class = "btn" > 4 < /button> <
+        button id = "btn5"
+        class = "btn" > 5 < /button> <
+        button id = "btn6"
+        class = "btn" > 6 < /button> <
+        button id = "btn7"
+        class = "btn" > 7 < /button> <
+        button id = "btn8"
+        class = "btn" > 8 < /button> <
+        button id = "btn9"
+        class = "btn" > 9 < /button> < /
+        div > <
+            script src = "js/buttonsGrid.js"
+        type = "text/javascript" > < /script> < /
+        body > <
+            /html>
+
+
+        // buttonsGrid.css
+        .btn {
+            width: 30 % ;
+            height: 48 px;
+            font - size: 24 px;
+        }
+
+        .btnContainer {
+            width: 75 %
+        }
+        /* #btn2{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn3{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn4{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn5{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn6{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn7{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn8{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        }
+        #btn9{
+            width:30%;
+            height:48px;
+            font-size:24px;
+        } */
+
+
+
+        // buttonsGrid.js
+
+        // var btn = document.createElement("Button")
+        // btn.innerHTML = "0";
+
+        // document.body.appendChild(btn);
+
+        let btn5 = document.getElementById("btn5");
+
+        btn5.onclick = function() {
+            var first = btn1.innerHTML
+            btn1.innerHTML = btn4.innerHTML;
+            btn4.innerHTML = btn7.innerHTML;
+            btn7.innerHTML = btn8.innerHTML;
+            btn8.innerHTML = btn9.innerHTML;
+            btn9.innerHTML = btn6.innerHTML;
+            btn6.innerHTML = btn3.innerHTML;
+            btn3.innerHTML = btn2.innerHTML;
+            btn2.innerHTML = first;
+
+        }
+
+
+        // Day 9: Binary Calculator
+
+        // index  .html
+
+        <!-- Enter your HTML code here -->
+        <
+        !DOCTYPE html >
+            <
+            html >
+            <
+            head >
+            <
+            meta charset = "utf-8" >
+            <
+            title > Binary Calculator < /title> <
+        link rel = "stylesheet"
+        href = "css/binaryCalculator.css"
+        type = "text/css" >
+            <
+            /head> <
+        body >
+            <
+            div id = "container" >
+            <
+            div id = "res" > < /div> <
+        div id = "btns" >
+            <
+            button id = "btn0"
+        onclick = "clickzero() " > 0 < /button> <
+        button id = "btn1"
+        onclick = "clickone()" > 1 < /button> <
+        button id = "btnClr"
+        onclick = "clickClear()" > C < /button> <
+        button id = "btnEql"
+        onclick = "clickEqual()" >= < /button> <
+        button id = "btnSum"
+        onclick = "clickSum()" > + < /button> <
+        button id = "btnSub"
+        onclick = "clickSub()" > - < /button> <
+        button id = "btnMul"
+        onclick = "clickMul()" > * < /button> <
+        button id = "btnDiv"
+        onclick = "clickDiv()" > /</button >
+            <
+            /div> < /
+        div > <
+            script src = "js/binaryCalculator.js"
+        type = "text/javascript" > < /script> < /
+        body > <
+            /html>
+
+
+
+        // binaryCalculator .css
+
+        body {
+            width: 33 % ;
+        }
+
+
+        #
+        res {
+            background - color: lightgray;
+            border: solid;
+            height: 48 px;
+            font - size: 20 px;
+
+
+
+        }#
+        btns button {
+            width: 25 % ;
+            height: 36 px;
+            font - size: 18 px;
+            margin: 0;
+            float: left;
+        }
+
+        #
+        btn0, #btn1 {
+            background - color: lightgreen;
+            color: brown;
+        }
+
+        #
+        btnClr, #btnEql {
+            background - color: darkgreen;
+            color: white;
+        }#
+        btnSum, #btnSub, #btnMul, #btnDiv {
+            background - color: black;
+            color: red;
+        }
+        /* .btn{
+            width: 25%;
+            height:36px;
+            font-size:18px;
+            margin:0px;
+             float: left;
+           
+        } */
+
+
+
+        // binaryCalculator.js
+
+        let elem = document.getElementById("res");
+
+        function checkOperator() {
+            if (elem.innerHTML.endswith('+') || elem.innerHTML.endswith('-') || elem.innerHTML.endswith('*') || elem.innerHTML.endswith('/')) {
+                return true;
+            }
+        }
+
+        function clickZero() {
+            elem.innerHTML += '0';
+        }
+
+        function clickOne() {
+            elem.innerHTML += '1';
+        }
+
+        function clickSum() {
+            if (elem.innerHTML != 0 && !checkOperator()) {
+                elem.innerHTML += '+';
+            }
+
+        }
+
+        function clickSub() {
+            if (elem.innerHTML != 0 && !checkOperator()) {
+                elem.innerHTML += '-';
+            }
+
+        }
+
+        function clickMul() {
+            if (elem.innerHTML != 0 && !checkOperator()) {
+                elem.innerHTML += '*';
+            }
+
+        }
+
+        function clickDiv() {
+            if (elem.innerHTML != 0 && !checkOperator()) {
+                elem.innerHTML += '/';
+            }
+
+        }
+
+        function clickClr() {
+            elem.innerHTML = '';
+        }
+
+        function clickEql() {
+            if (!checkOperator()) {
+                let re = /\d+/g
+                let re2 = /[\+\-\*\/]+/g
+                let numbers = elem.innerHTML.match(re);
+                let operations = elem.innerHTML.match(re2);
+                while (operations.length > 0) {
+                    if (operations.include('+')) {
+                        let indexOfMul = operations.indexOf('*');
+                        let mul = (indexOfMul != 0 ? parseInt(numbers[indexOfMul - 1], 2) :
+                            parseInt(numbers[indexOfMul], 2)) * parseInt(numbers[indexOfMul + 1], 2);
+                        numbers.splice(indexOfMul, 2);
+                        numbers.splice(indexOfMul, 0, mul.toString(2));
+                        operations.splice(indexOfMul, 1);
+                    } else if (operations.includes('/')) {
+                        let indexOfDiv = operations.indexOf('/');
+                        let division = (indexOfDiv != 0 ? parseInt(numbers[indexOfDiv - 1], 2) :
+                            parseInt(numbers[indexOfDiv], 2)) / parseInt(numbers[indexOfDiv + 1], 2);
+                        numbers.splice(indexOfDiv, 2);
+                        numbers.splice(indexOfDiv, division.toString(2));
+                        operations.splice(indexOfDiv, 1);
+                    } else if (operations.include('+')) {
+                        let indexOfSum = operations.indexOf('+ ');
+                        let sum = (indexOfSum != 0 ? parseInt(numbers[indexOfSum - 1], 2) :
+                            parseInt(numbers[indexOfSum], 2)) + parseInt(numbers[indexOfSum + 1], 2);
+                        numbers.splice(indexOfSum, 2);
+                        numbers.splice(indexOfSum, 0, sum.toString(2));
+                        operations.splice(indexOfSum, 1);
+                    } else {
+                        let indexOfSub = operations.indexOf('-');
+                        let sub = (indexOfSub != 0 ? parseInt(numbers[indexOfSub - 1], ) :
+                            parseInt(numbers[indexOfSub], 2)) - parseInt(numbers[indexOfSub + 1], 2);
+                        numbers.splice(indexOfSub, 2);
+                        numbers.splice(indexOfSub, 0, sub.toString(2));
+                        operations.splice(indexOfSub, 1);
+
+                    }
+
+                }
+                elem.innerHTML = numbers[0].toString(2);
+            } else {
+                alert("Line must ends with number.");
+            }
         }
